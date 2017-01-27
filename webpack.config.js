7c97043846fcaaa6b9be00cdd72b393c98fbeb5b
Rodrigo Díez Villamuera <rodrigo@rodrigodiez.io>
2017-01-27 12:02:47 +0000

    Sucessfuly using bootstrap css now

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: ['./src/app.js', './src/app.style.js'],
     output: {
         filename: 'js/app.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-    new HtmlWebpackPlugin(),
-    new webpack.ProvidePlugin({
+      new HtmlWebpackPlugin({
+            template: './src/app.html'
+        }),
+      new webpack.ProvidePlugin({
             jQuery: 'jquery',
             $: 'jquery',
             jquery: 'jquery'
         }),
-    new CleanWebpackPlugin(['dist'], {
+      new CleanWebpackPlugin(['dist'], {
             verbose: true,
             dry: false,
         })
-  ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+          ]
+    }
 }
